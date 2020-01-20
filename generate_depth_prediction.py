618a0a2bd0e83752e32c186a64b952576f641ac1
TWJianNuo <twjiannuo@gmail.com>
2020-01-19 21:10:45 -0500

    changes

--- generate_depth_prediction.py
+++ generate_depth_prediction.py
@@ -8,10 +8,242 @@ import torch
 from torch.utils.data import DataLoader
 
 from layers import disp_to_depth
-from options import MonodepthOptions
 from utils import *
 import datasets
 import networks
+
+
+
+
+import time
+
+
+import os
+import argparse
+import pathlib
+
+
+file_dir = os.path.dirname(__file__)  # the directory that options.py resides in
+
+
+class TmpOptions:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Monodepthv2 options")
+
+        # PATHS
+        self.parser.add_argument("--data_path",
+                                 type=str,
+                                 help="path to the training data",
+                                 default=os.path.join(file_dir, "kitti_data"))
+        self.parser.add_argument("--log_dir",
+                                 type=str,
+                                 help="log directory",
+                                 default=os.path.join(str(pathlib.Path().absolute()), "tmp"))
+
+        # TRAINING options
+        self.parser.add_argument("--model_name",
+                                 type=str,
+                                 help="the name of the folder to save the model in",
+                                 default="mdp")
+        self.parser.add_argument("--split",
+                                 type=str,
+                                 help="which training split to use",
+                                 default="eigen_zhou")
+        self.parser.add_argument("--num_layers",
+                                 type=int,
+                                 help="number of resnet layers",
+                                 default=18,
+                                 choices=[18, 34, 50, 101, 152])
+        self.parser.add_argument("--dataset",
+                                 type=str,
+                                 help="dataset to train on",
+                                 default="kitti",
+                                 choices=["kitti", "kitti_odom", "kitti_depth", "kitti_test"])
+        self.parser.add_argument("--png",
+                                 help="if set, trains from raw KITTI png files (instead of jpgs)",
+                                 action="store_true")
+        self.parser.add_argument("--height",
+                                 type=int,
+                                 help="input image height",
+                                 default=192)
+        self.parser.add_argument("--width",
+                                 type=int,
+                                 help="input image width",
+                                 default=640)
+        self.parser.add_argument("--disparity_smoothness",
+                                 type=float,
+                                 help="disparity smoothness weight",
+                                 default=1e-3)
+        self.parser.add_argument("--scales",
+                                 nargs="+",
+                                 type=int,
+                                 help="scales used in the loss",
+                                 default=[0, 1, 2, 3])
+        self.parser.add_argument("--min_depth",
+                                 type=float,
+                                 help="minimum depth",
+                                 default=0.1)
+        self.parser.add_argument("--max_depth",
+                                 type=float,
+                                 help="maximum depth",
+                                 default=100.0)
+        self.parser.add_argument("--use_stereo",
+                                 help="if set, uses stereo pair for training",
+                                 action="store_true")
+        self.parser.add_argument("--frame_ids",
+                                 nargs="+",
+                                 type=int,
+                                 help="frames to load",
+                                 default=[0, -1, 1])
+
+        self.parser.add_argument("--predins",
+                                 help="if set, predict instance",
+                                 action="store_true")
+        self.parser.add_argument("--detect_path",
+                                 type=str,
+                                 help="path of detection labels",
+                                 default="")
+        self.parser.add_argument("--noshuffle",
+                                 action="store_true")
+        self.parser.add_argument("--loadSeman",
+                                 action="store_true")
+        self.parser.add_argument("--noAug",
+                                 action="store_true")
+        self.parser.add_argument("--loadPose",
+                                 action="store_true")
+        self.parser.add_argument("--loadPredDepth",
+                                 action="store_true")
+        self.parser.add_argument("--predDepthPath",
+                                 type=str)
+        self.parser.add_argument("--save_dir",
+                                 type=str)
+
+
+        # OPTIMIZATION options
+        self.parser.add_argument("--batch_size",
+                                 type=int,
+                                 help="batch size",
+                                 default=12)
+        self.parser.add_argument("--learning_rate",
+                                 type=float,
+                                 help="learning rate",
+                                 default=1e-4)
+        self.parser.add_argument("--num_epochs",
+                                 type=int,
+                                 help="number of epochs",
+                                 default=20)
+        self.parser.add_argument("--scheduler_step_size",
+                                 type=int,
+                                 help="step size of the scheduler",
+                                 default=15)
+
+        # ABLATION options
+        self.parser.add_argument("--v1_multiscale",
+                                 help="if set, uses monodepth v1 multiscale",
+                                 action="store_true")
+        self.parser.add_argument("--avg_reprojection",
+                                 help="if set, uses average reprojection loss",
+                                 action="store_true")
+        self.parser.add_argument("--disable_automasking",
+                                 help="if set, doesn't do auto-masking",
+                                 action="store_true")
+        self.parser.add_argument("--predictive_mask",
+                                 help="if set, uses a predictive masking scheme as in Zhou et al",
+                                 action="store_true")
+        self.parser.add_argument("--no_ssim",
+                                 help="if set, disables ssim in the loss",
+                                 action="store_true")
+        self.parser.add_argument("--weights_init",
+                                 type=str,
+                                 help="pretrained or scratch",
+                                 default="pretrained",
+                                 choices=["pretrained", "scratch"])
+        self.parser.add_argument("--pose_model_input",
+                                 type=str,
+                                 help="how many images the pose network gets",
+                                 default="pairs",
+                                 choices=["pairs", "all"])
+        self.parser.add_argument("--pose_model_type",
+                                 type=str,
+                                 help="normal or shared",
+                                 default="separate_resnet",
+                                 choices=["posecnn", "separate_resnet", "shared"])
+
+        # SYSTEM options
+        self.parser.add_argument("--no_cuda",
+                                 help="if set disables CUDA",
+                                 action="store_true")
+        self.parser.add_argument("--num_workers",
+                                 type=int,
+                                 help="number of dataloader workers",
+                                 default=12)
+
+        # LOADING options
+        self.parser.add_argument("--load_weights_folder",
+                                 type=str,
+                                 help="name of model to load")
+        self.parser.add_argument("--models_to_load",
+                                 nargs="+",
+                                 type=str,
+                                 help="models to load",
+                                 default=["encoder", "depth", "pose_encoder", "pose"])
+
+        # LOGGING options
+        self.parser.add_argument("--log_frequency",
+                                 type=int,
+                                 help="number of batches between each tensorboard log",
+                                 default=250)
+        self.parser.add_argument("--save_frequency",
+                                 type=int,
+                                 help="number of epochs between each save",
+                                 default=1)
+
+        # EVALUATION options
+        self.parser.add_argument("--eval_stereo",
+                                 help="if set evaluates in stereo mode",
+                                 action="store_true")
+        self.parser.add_argument("--eval_mono",
+                                 help="if set evaluates in mono mode",
+                                 action="store_true")
+        self.parser.add_argument("--disable_median_scaling",
+                                 help="if set disables median scaling in evaluation",
+                                 action="store_true")
+        self.parser.add_argument("--pred_depth_scale_factor",
+                                 help="if set multiplies predictions by this number",
+                                 type=float,
+                                 default=1)
+        self.parser.add_argument("--ext_disp_to_eval",
+                                 type=str,
+                                 help="optional path to a .npy disparities file to evaluate")
+        self.parser.add_argument("--eval_split",
+                                 type=str,
+                                 default="eigen",
+                                 choices=[
+                                    "eigen", "eigen_benchmark", "benchmark", "odom_9", "odom_10"],
+                                 help="which split to run eval on")
+        self.parser.add_argument("--save_pred_disps",
+                                 help="if set saves predicted disparities",
+                                 action="store_true")
+        self.parser.add_argument("--no_eval",
+                                 help="if set disables evaluation",
+                                 action="store_true")
+        self.parser.add_argument("--eval_eigen_to_benchmark",
+                                 help="if set assume we are loading eigen results from npy but "
+                                      "we want to evaluate using the new benchmark.",
+                                 action="store_true")
+        self.parser.add_argument("--eval_out_dir",
+                                 help="if set will output the disparities to this folder",
+                                 type=str)
+        self.parser.add_argument("--post_process",
+                                 help="if set will perform the flipping post processing "
+                                      "from the original monodepth paper",
+                                 action="store_true")
+
+    def parse(self):
+        self.options = self.parser.parse_args()
+        return self.options
+
+
 
 cv2.setNumThreads(0)  # This speeds up evaluation 5x on our unix systems (OpenCV 3.3.1)
 
@@ -55,6 +287,30 @@ def batch_post_process_disparity(l_disp, r_disp):
     r_mask = l_mask[:, :, ::-1]
     return r_mask * l_disp + l_mask * r_disp + (1.0 - l_mask - r_mask) * m_disp
 
+def get_entry_from_path(imgpath):
+    comps = imgpath.split('/')
+    if comps[-3] == 'image_02':
+        direct = 'l'
+    else:
+        direct = 'r'
+    entry = comps[-5] + '/' + comps[-4] + ' ' + comps[-1].split('.')[0] + ' ' + direct + '\n'
+    return entry
+
+def collect_all_entries(folder):
+    import glob
+    # folder = '/media/shengjie/other/sceneUnderstanding/monodepth2/kitti_data/kitti_raw'
+    subfolders = [f.path for f in os.scandir(folder) if f.is_dir()]
+    entries = list()
+    for seqs_clusts in subfolders:
+        seqs = [f.path for f in os.scandir(seqs_clusts) if f.is_dir()]
+        for seq in seqs:
+            imgFolder_02 = os.path.join(seq, 'image_02', 'data')
+            imgFolder_03 = os.path.join(seq, 'image_03', 'data')
+            for imgpath in glob.glob(imgFolder_02 + '/*.png'):
+                entries.append(get_entry_from_path(imgpath))
+            for imgpath in glob.glob(imgFolder_03 + '/*.png'):
+                entries.append(get_entry_from_path(imgpath))
+    return entries
 
 def evaluate(opt):
     """Evaluates a pretrained model using a specified test set
@@ -62,6 +318,7 @@ def evaluate(opt):
     MIN_DEPTH = 1e-3
     MAX_DEPTH = 80
 
+    ts = time.time()
     if opt.ext_disp_to_eval is None:
 
         opt.load_weights_folder = os.path.expanduser(opt.load_weights_folder)
@@ -71,7 +328,8 @@ def evaluate(opt):
 
         print("-> Loading weights from {}".format(opt.load_weights_folder))
 
-        filenames = readlines(os.path.join(splits_dir, opt.split, "train_files.txt"))
+        # filenames = readlines(os.path.join(splits_dir, opt.split, "train_files.txt"))
+        filenames = collect_all_entries(opt.data_path)
         encoder_path = os.path.join(opt.load_weights_folder, "encoder.pth")
         decoder_path = os.path.join(opt.load_weights_folder, "depth.pth")
 
@@ -100,7 +358,7 @@ def evaluate(opt):
         print("-> Computing predictions with size {}x{}".format(
             encoder_dict['width'], encoder_dict['height']))
 
-        save_dir = "/media/shengjie/other/sceneUnderstanding/monodepth2/kitti_data/monodepth2_depth_prediction"
+        save_dir = opt.save_dir
         print("-> Saving out predictions to {}".format(save_dir))
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
@@ -130,13 +388,14 @@ def evaluate(opt):
 
                 for k in range(depth.shape[0]):
                     comps = filenames[imgCount].split(" ")
-                    save_folder = os.path.join(save_dir, comps[0], mapping[comps[2]])
+                    save_folder = os.path.join(save_dir, comps[0], mapping[comps[2][0]])
                     os.makedirs(save_folder, exist_ok=True)
                     save_path = os.path.join(save_folder, comps[1] + '.png')
                     cv2.imwrite(save_path, depth[k, :, :])
+                    te = time.time()
                     imgCount = imgCount + 1
-                print("%d finished" % idx)
+                print("%d finished, %f hours left" % (idx, (te - ts) / imgCount * (len(filenames) - imgCount) / 60 / 60))
 
 if __name__ == "__main__":
-    options = MonodepthOptions()
+    options = TmpOptions()
     evaluate(options.parse())
