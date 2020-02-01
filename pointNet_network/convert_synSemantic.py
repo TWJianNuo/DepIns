05de8e01dad2f5c52c864b496fa4a25af1bd6afc
TWJianNuo <twjiannuo@gmail.com>
2020-01-31 20:43:13 -0500

    changes

--- pointNet_network/convert_synSemantic.py
+++ pointNet_network/convert_synSemantic.py
@@ -1,4 +1,9 @@
-import os
+from __future__ import absolute_import, division, print_function
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--syn_root', type=str)
