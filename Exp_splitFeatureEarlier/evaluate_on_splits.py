dbda7994e61625a6abab7e21311fd8dfa9eaacd7
TWJianNuo <twjiannuo@gmail.com>
2020-07-06 09:05:50 -0400

    add eval

--- Exp_splitFeatureEarlier/evaluate_on_splits.py
+++ Exp_splitFeatureEarlier/evaluate_on_splits.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
-import os
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
 import cv2
 import numpy as np
 
