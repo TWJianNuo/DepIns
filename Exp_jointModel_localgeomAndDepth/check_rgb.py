3b0d7e2c534f13f3788554af4a5193bd0f28aff4
TWJianNuo <twjiannuo@gmail.com>
2020-08-17 09:53:34 -0400

    improve reproducibility

--- Exp_jointModel_localgeomAndDepth/check_rgb.py
+++ Exp_jointModel_localgeomAndDepth/check_rgb.py
@@ -9,24 +9,27 @@ import numpy as np
 
 from shutil import copyfile
 
-bckpath = '/home/shengjie/Documents/Data/Kitti/kitti_raw/kitti_data'
+import time
+
+bckpath = '/media/shengjie/c9c81c9f-511c-41c6-bfe0-2fc19666fb32/Data/kitti/kitti_raw'
 
 train_fpath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "splits", "eigen_full", "train_files.txt")
 test_fpath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "splits", "eigen", "test_files.txt")
 ck_filenames = readlines(train_fpath) + readlines(test_fpath)
 
 dirmapping = {'l':'image_02', 'r':'image_03'}
+
+count = 0
+st = time.time()
 for entry in ck_filenames:
     comps = entry.split(' ')
     filepath = os.path.join('/home/shengjie/Documents/Data/Kitti/kitti_raw/kitti_data', comps[0], dirmapping[comps[2]], 'data', comps[1].zfill(10) + '.png')
-    if not os.path.isfile(filepath):
-        print("File %s missing" % filepath)
-    else:
-        try:
-            with open(filepath, 'rb') as f:
-                with pil.open(f) as img:
-                    rgb = img.convert('RGB')
-        except:
-            print("Problematic: %s" % entry)
-            replacepath = os.path.join(bckpath, comps[0], dirmapping[comps[2]], 'data', comps[1].zfill(10) + '.png')
-            copyfile(src=replacepath, dst=filepath)
+    replacepath = os.path.join(bckpath, comps[0], dirmapping[comps[2]], 'data', comps[1].zfill(10) + '.png')
+    copyfile(src=replacepath, dst=filepath)
+    count = count + 1
+
+    avetime = (time.time() - st) / count
+    resthrs = avetime * (len(ck_filenames) - count) / 60 / 60
+    print("Rest time is %f hours, finished %d" % (resthrs, count))
+
+
